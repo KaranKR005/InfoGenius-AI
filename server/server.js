df9ebead590958710f68bd628f1f97e3af97afb8
Karan Ram <Karanram73005@gmail.com>
2023-10-28 03:05:16 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -2,9 +2,13 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 import { Configuration, OpenAIApi } from 'openai';
-import { MongoClient } from 'mongodb';
+import pkg from 'mongodb';
+const { MongoClient } = pkg;
+
+const conversationHistory = [];
 
 import { generateCustomResponse } from './custom_msg.js';
+generateCustomResponse;
 
 dotenv.config();
 
@@ -18,58 +22,87 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// MongoDB Connection
 const client = new MongoClient(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    useNewUrlParser: true, // Deprecated option
+    useUnifiedTopology: true, // Deprecated option
 });
 
+// Connect to MongoDB
 client.connect().then(() => {
     console.log('Connected to MongoDB');
 }).catch(error => {
     console.error('Error connecting to MongoDB:', error);
 });
 
-// Store user-specific conversation history
-const conversationHistory = {};
+// Generate a unique collection name using a timestamp
+const getUniqueCollectionName = () => {
+    const timestamp = Date.now();
+    return `ChatHistory_${timestamp}`;
+};
+
+app.get('/', async (req, res) => {
+    res.status(200).send({
+        message: 'Hello from InfoGeniusAI'
+    });
+});
 
 app.post('/start-chat', async (req, res) => {
     try {
         const userName = req.body.name;
-        const userMessage = req.body.message;
 
         // Generate a collection name based on the user's name
-        const collectionName = userName.replace(/\s+/g, '_'); // Replace spaces with underscores
+        const collectionName = userName.replace(/\s/g, '_'); // Replace spaces with underscores
 
-        // Retrieve or create user's conversation history
-        if (!conversationHistory[collectionName]) {
-            conversationHistory[collectionName] = [];
+        // Store chat data in a new MongoDB collection
+        const database = client.db('ChatDB');
+        const collection = database.collection(collectionName);
+
+        // // Store chat data in a new MongoDB collection
+        // const database = client.db('ChatDB');
+        // const collection = database.collection(collectionName);
+
+        const chatData = {
+            user: userMessage,
+            bot: customResponse || "",
+            timestamp: new Date(),
+        };
+
+        await collection.insertOne(chatData);
+
+        if (customResponse !== null) {
+            // If there's a custom response, send it directly
+            res.status(200).send({
+                bot: customResponse,
+            });
+        } else {
+            // Otherwise, continue the conversation with the AI (using gpt-3.5-turbo)
+            const response = await openai.createCompletion({
+                model: "text-davinci-003",
+                prompt: conversationHistory.map(entry => entry.message).join(' '), // Join conversation history for context
+                temperature: 0.2,
+                max_tokens: 3000,
+                top_p: 1,
+                frequency_penalty: 0.5,
+                presence_penalty: 0,
+            });
+    
+            // Extract bot response from OpenAI API response
+            const botResponse = response.data.choices[0].text;
+    
+            // Store the OpenAI generated response in the chatData object
+            chatData.bot = botResponse;
+    
+            // Update the conversation history with bot response
+            conversationHistory.push({ role: 'bot', message: botResponse });
+    
+            // Update the MongoDB record with the OpenAI generated response
+            await collection.updateOne({ _id: chatData._id }, { $set: { bot: botResponse } });
+
+            res.status(200).send({
+                bot: botResponse,
+            });
         }
-
-        // Store user's message in conversation history
-        conversationHistory[collectionName].push({ role: 'user', message: userMessage });
-
-        // Continue the conversation with the AI (using gpt-3.5-turbo)
-        const response = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: conversationHistory[collectionName].map(entry => entry.message).join(' '), // Join conversation history for context
-            temperature: 0.2,
-            max_tokens: 3000,
-            top_p: 1,
-            frequency_penalty: 0.5,
-            presence_penalty: 0,
-        });
-
-        // Extract bot response from OpenAI API response
-        const botResponse = response.data.choices[0].text;
-
-        // Store the AI generated response in the conversation history
-        conversationHistory[collectionName].push({ role: 'bot', message: botResponse });
-
-        // Send the bot response back to the client
-        res.status(200).send({
-            bot: botResponse,
-        });
-        
     } catch (error) {
         console.error(error);
         res.status(500).send('Something went wrong: ' + error.message);
