16eed040da62a79014527756db6c287b1b304b8b
Karan Ram <Karanram73005@gmail.com>
2023-11-03 21:46:33 +0530

    first commit

--- client/script.js
+++ client/script.js
@@ -132,7 +132,7 @@ const handleSubmit = async (e) => {
     const dev = 'http://localhost:5000'
 
     try {
-        const response = await fetch('http://localhost:5000', {
+        const response = await fetch(live, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
